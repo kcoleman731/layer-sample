6573417bec2e0b3650ee0c4d62a39637b559ca43
Kevin Coleman <kcoleman731@gmail.com>
2014-11-08 13:33:56 -0800

    Initial Commit

---
+++ LayerPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayerPhoto
+//
+//  Created by Kevin Coleman on 11/8/14.
+//  Copyright (c) 2014 Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayerPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayerPhoto
+//
+//  Created by Kevin Coleman on 11/8/14.
+//  Copyright (c) 2014 Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
